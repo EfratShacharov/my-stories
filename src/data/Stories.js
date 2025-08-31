d3d2b500a39412afabade5f506f8773b8e3e5387
Efrat Shacharov <ersh42m@gmail.com>
2025-08-31 21:02:50 +0300

    Rename files

--- src/data/Stories.js
+++ src/data/Stories.js
@@ -4,14 +4,14 @@ const stories = [
       title: "סיפור ראשון",
       summary: "תקציר קצר של סיפור ראשון...",
       content: "זהו הטקסט המלא של הסיפור הראשון...",
-      pdf: "/stories/לא מוותרים! - סופי.pdf"
+      pdf: "/stories/Lo_Mevatrim.pdf"
     },
     {
       id: 2,
       title: "סיפור שני",
       summary: "תקציר קצר של סיפור שני...",
       content: "זהו הטקסט המלא של הסיפור השני...",
-      pdf: "/stories/הלוואי שנזכה - סופי.pdf"
+      pdf: "/stories/Halevai_Shenizkeh.pdf"
     },
   ];
   
